8f5448021bb2c4dbed109f4c8305ae99142f1f99
Anthony Lin <lin.anthonyh@gmail.com>
2024-11-18 02:36:48 -0800

    change hero section to use grid instead of absolute positioning for responsiveness

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -5,20 +5,25 @@ import { HeroWrapper } from './HeroWrapper'
 import { StaticHeroImage } from './StaticHeroImage'
 
 export const Hero: React.FC = () => {
+
   const renderLogo = () => {
     return (
-      <div className="flex flex-col items-center justify-center">
-        <Image
-          src="/images/logo-lg.png"
-          alt="Pixie Brows - Orange County Eyebrow Artist"
-          width={1775}
-          height={802}
-          className="w-11/12 md:w-full max-w-2xl md:max-w-3xl mx-auto animate-scale-in opacity-100 z-10 will-change-transform"
-          priority
-          sizes="100vh, 100vw"
-        />
-        <ScrollButton />
-      </div>
+      <ol className="h-full grid grid-rows-[1fr_auto_1fr] gap-1 justify-items-center grid-flow-col p-0 m-0 list-none text-white">
+        <li className="row-start-2">
+          <Image
+            src="/images/logo-lg.png"
+            alt="Pixie Brows - Orange County Eyebrow Artist"
+            width={1775}
+            height={802}
+            className="w-11/12 md:w-full max-w-xl min-w-80 md:max-w-2xl mx-auto animate-scale-in opacity-100 z-10 will-change-transform"
+            priority
+            sizes="100vh, 100vw"
+          />
+        </li>
+        <li className="mt-auto mb-56 md:mb-64">
+          <ScrollButton />
+        </li>
+      </ol>
     )
   }
   return (
--- src/components/ui/ScrollButton.tsx
+++ src/components/ui/ScrollButton.tsx
@@ -5,10 +5,10 @@ import { ChevronDownIcon } from 'lucide-react'
 
 export default function ScrollButton() {
   return (
-    <div className="absolute bottom-48 sm:bottom-64 left-0 right-0 flex items-center justify-center">
+    <div className="flex items-center justify-center">
       <button
         onClick={() => scrollToSection('#services')}
-        className="transform border-solid border-2 bg-transparent hover:bg-white/25 hover:bg-white/ text-white rounded-full py-2 px-6 transition-all duration-300 ease-in-out flex items-center space-x-2 shadow-md hover:shadow-lg" 
+        className="transform border-solid border-2 bg-transparent hover:bg-white/25 hover:bg-white/ text-white rounded-full py-2 px-6 transition-all duration-300 ease-in-out flex items-center space-x-2 shadow-md hover:shadow-lg"
         aria-label="Scroll to Services"
       >
         <span className="text-md md:text-xl">Explore Services</span>
